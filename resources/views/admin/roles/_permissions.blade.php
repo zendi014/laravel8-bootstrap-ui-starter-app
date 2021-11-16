82272edcf2acaeeeb9563746bc430d3f6a3fe5fe
zendi014 <48636084+zendi014@users.noreply.github.com>
2021-11-16 21:57:38 +0700

    Update _permissions.blade.php

--- resources/views/admin/roles/_permissions.blade.php
+++ resources/views/admin/roles/_permissions.blade.php
@@ -1,10 +1,11 @@
 <div class="row">
+
     @foreach($permissions as $perm)
         <?php
             $per_found = null;
 
-            if( isset($role) ) {
-                $per_found = $role->hasPermissionTo($perm->name);
+            if( isset($roles) ) {
+                $per_found = $user->hasPermissionTo($perm->name);
             }
 
             if( isset($user)) {
@@ -15,11 +16,20 @@
         ?>
         <div class="col-sm-3">
             <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="permissions[]" id="{{ $perm->id }}" value="{{ $perm->name }}" {{  ($per_found == true ? ' checked' : '') }} {{ $disabled ?? '' }}>
+                <input 
+                    class="form-check-input" 
+                    type="checkbox" 
+                    name="permissions[]" 
+                    id="{{ $perm->id }}" 
+                    value="{{ $perm->name }}" 
+                    {{  (is_null($per_found) ? '' : 'checked') }} {{ $disabled ?? '' }}
+                    {{  ($per_found != null ? ' checked' : '') }} {{ $disabled ?? '' }}
+                >
                 <label class="form-check-label {{ Str::contains($perm->name, 'delete') ? 'text-danger' : '' }}" for="{{ $perm->id }}">
                     {{ $labelName }}
                 </label>
             </div>
         </div>
+        
     @endforeach
 </div>
